d2c9f2efa89691f982ed7dcaa43d3d74a47f1050
kharisma tara mukti <kharismataram@gmail.com>
2023-08-12 10:42:00 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('landing.index');
-});
-Route::get('/pricing', function () {
-    return view('pricing');
-});
+
+Route::resource('/', LandingController::class);
